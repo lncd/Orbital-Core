c0332f1a6b527db13e182442b4da5ab0f72fae16
Nick Jackson <nijackson@lincoln.ac.uk>
2011-12-07 15:34:43 +0000

    Added basics of ping command

--- application/controllers/splash.php
+++ application/controllers/splash.php
@@ -9,6 +9,7 @@ class Splash extends CI_Controller {
 
 	function index()
 	{
+		$this->config->load('orbital');
 		$this->load->view('splash');
 	}
 }
--- application/views/splash.php
+++ application/views/splash.php
@@ -29,6 +29,10 @@
 
 	<h1><img src="<?php echo base_url(); ?>img/logo.png" title="Orbital" alt="Orbital Logo"></h1>
 	
+	<p>This is Orbital Core version <?php echo $this->config->item('orbital_core_version'); ?> at <?php echo $this->config->item('orbital_institution_name'); ?>.</p>
+	
+	<p>Want more? Hit up the API's 'ping' command by firing a HTTP GET to <a href="<?php echo site_url('core/ping'); ?>"><?php echo site_url('core/ping'); ?></a></p>
+	
 	<p><a href="https://github.com/lncd/Orbital-Core">Orbital Core on Github</a></p>
 	<p><a href="http://orbital.blogs.lincoln.ac.uk/">Orbital Blog</a></p>
 
