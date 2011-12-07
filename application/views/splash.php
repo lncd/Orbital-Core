d9647badeacfd964f64ee30c5a7dc5ac506ab656
Nick Jackson <nijackson@lincoln.ac.uk>
2011-12-07 15:17:08 +0000

    Initial CodeIgniter, PS REST library, splash page.

---
+++ application/views/splash.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<title>Orbital Core</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Spinnaker' rel='stylesheet' type='text/css'>
+
+	<style type="text/css">
+	
+		body {
+			background: #000 url(<?php echo base_url(); ?>img/bg.png);
+			color: #FFF;
+			font-family: 'Spinnaker', sans-serif;
+			text-align: center;
+			font-size: 1.2em;
+			margin-top: 100px;
+		}
+		
+		a {
+			color: #F5D89A;
+		}
+	
+	</style>
+	
+</head>
+<body>
+
+	<h1><img src="<?php echo base_url(); ?>img/logo.png" title="Orbital" alt="Orbital Logo"></h1>
+	
+	<p><a href="https://github.com/lncd/Orbital-Core">Orbital Core on Github</a></p>
+	<p><a href="http://orbital.blogs.lincoln.ac.uk/">Orbital Blog</a></p>
+
+</body>
+</html>
