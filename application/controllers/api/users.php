1f06cf8f8f0cc52cea2a09cb3dccdd822112730f
Nick Jackson <nijackson@lincoln.ac.uk>
2012-01-16 15:35:43 +0000

    Users, ho!

---
+++ application/controllers/api/users.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH.'/libraries/REST_Controller.php';
+require APPPATH.'/libraries/Orbital_Controller.php';
+
+class Users extends Orbital_Controller
+{
+	function list_get()
+    {
+        $user = $this->mongo_db->get('users');
+    	
+        if($user)
+        {
+            $this->response($user, 200); // 200 being the HTTP response code
+        }
+
+        else
+        {
+            $this->response(array('error' => 'User could not be found'), 404);
+        }
+    }
+}
