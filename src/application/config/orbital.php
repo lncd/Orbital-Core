b5548f52a9fdff5bd57210d9600da6c86dd0fe1c
Nick Jackson <nijackson@lincoln.ac.uk>
2012-01-19 11:26:35 +0000

    Trying to get a more 'proper' structure in place for builds, testing and distribution.

---
+++ src/application/config/orbital.php
@@ -0,0 +1,73 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Orbital Configuration File
+ *
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array.
+ *
+ * @package		Orbital
+ * @subpackage	Core
+ * @category	Configuration
+ * @author		Nick Jackson
+ * @link		https://github.com/lncd/Orbital-Core
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Institution Name
+|--------------------------------------------------------------------------
+|
+| The name of the institution responsible for this Orbital Core
+| installation.
+|
+*/
+$config['orbital_institution_name'] = '##ORBITAL_INSTITUTION_NAME##';
+
+/*
+|--------------------------------------------------------------------------
+| Operation Mode
+|--------------------------------------------------------------------------
+|
+| Orbital Core's behaviour mode.
+|
+| Accepted values:
+|   maintenance	- Respond to all requests with the maintenance status
+|                 message.
+|   readonly - Only accept read requests, respond to all write requests
+|              with the readonly status message.
+|   normal - Normal behaviour.
+|
+*/
+$config['orbital_operation_mode'] = 'normal';
+
+/*
+|--------------------------------------------------------------------------
+| Status Messages
+|--------------------------------------------------------------------------
+|
+| Messages to be returned during various operation modes.
+|
+| Message types:
+|   maintenance	- During maintenance all requests will be responded to with
+|                 this message.
+|   readonly - During read-only operation all requests will be responded to
+|              with this message.
+|
+*/
+
+$config['orbital_status_message_maintenance'] = 'Orbital is currently undergoing maintenance.';
+$config['orbital_status_message_readonly'] = 'Orbital is currently in read-only mode.';
+
+/*
+|--------------------------------------------------------------------------
+| Orbital Core Version
+|--------------------------------------------------------------------------
+|
+| The version of the Orbital Core.
+|
+*/
+$config['orbital_core_version'] = '0.0.1';
+
+/* End of file orbital.php */
+/* Location: ./application/config/orbital.php */
