72791689658287e25f74d5d382096fd7f6d9f403
Nick Jackson <nijackson@lincoln.ac.uk>
2012-02-09 11:38:21 +0000

    Auth Demo work

--- src/application/libraries/authentication/Auth_demo.php
+++ src/application/libraries/authentication/Auth_demo.php
@@ -1,17 +1,19 @@
 <?php
 
-class Auth_demo() {
+class Auth_demo {
 
 	function signin()
 	{
 	
-		echo 'Sign In Demo';
+		echo '<h1>Sign In Demo</h1>';
+		
+		echo '<p><a href="http://core.orbital.lncd.org/auth/callback/demo?u=orbital@lncd.org">Sign in as orbital@lncd.org</a></p>'.
 	
 	}
 	
 	function callback()
 	{
-	
+		echo 'SIGN IN ' . $this->input->get('u');
 	}
 
 }
