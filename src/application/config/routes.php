0e70b490ae7a4fbb8ed54b1e06bb07b9f804ac62
Nick Jackson <nijackson@lincoln.ac.uk>
2012-02-09 11:33:42 +0000

    Dramatically reducing stupidity.
    Function name same as the class is loaded as a constructor, you fool.

--- src/application/config/routes.php
+++ src/application/config/routes.php
@@ -41,8 +41,5 @@
 $route['default_controller'] = "splash";
 $route['404_override'] = '';
 
-$route['signin/callback/(:any)'] = "signin/callback/$1";
-$route['signin/(:any)'] = "signin/signin/$1";
-
 // End of file routes.php
 // Location: ./config/routes.php
---
+++ src/application/controllers/auth.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Authentication
+ *
+ * Marshals sign-in request to the appropriate sign-in endpoint.
+ *
+ * @package		Orbital
+ * @subpackage  Core
+ * @author		Nick Jackson
+ * @link		https://github.com/lncd/Orbital-Core
+ */
+
+class Auth extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function signin($endpoint)
+	{
+		if ($this->load->library('authentication/Auth_' . $endpoint))
+		{
+			$this->Auth_{$endpoint}->signin();
+		}
+		else
+		{
+			show_404();
+		}
+	}
+	
+	function callback($endpoint)
+	{
+		if ($this->load->library('authentication/Auth_' . $endpoint))
+		{
+			$this->Auth_{$endpoint}->callback();
+		}
+		else
+		{
+			show_404();
+		}
+	}
+}
+
+// EOF
