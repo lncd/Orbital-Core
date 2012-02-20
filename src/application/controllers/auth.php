dcaf5b9322d0b88c636a2276b1bfa1b3f5b710d8
Nick Jackson <nijackson@lincoln.ac.uk>
2012-02-20 16:02:36 +0000

    Auth adjustments

--- src/application/controllers/auth.php
+++ src/application/controllers/auth.php
@@ -18,6 +18,15 @@ class Auth extends CI_Controller {
 		parent::__construct();
 	}
 
+	/**
+	 * Sign-In Marshalling
+	 *
+	 * Builds state variable and routes sign-in requests to the appropriate
+	 * authentication library.
+	 *
+	 * @property $endpoint The designated sign-in endpoint.
+	 */
+
 	function signin($endpoint)
 	{
 		$this->load->library('authentication/Auth_' . $endpoint, '', 'auth_endpoint');
@@ -25,12 +34,32 @@ class Auth extends CI_Controller {
 		$this->auth_endpoint->signin($state);
 	}
 	
+	/**
+	 * Callback marshalling
+	 *
+	 * Routes sign-in callbacks to the appropriate authentication library,
+	 * validates the user data, performs any necessary user creation, builds
+	 * the OAuth response for the client, and redirects accordingly.
+	 *
+	 * @property $endpoint The designated sign-in endpoint.
+	 */
+	
 	function callback($endpoint)
 	{
 		$this->load->library('authentication/Auth_' . $endpoint, '', 'auth_endpoint');
 		if ($response = $this->auth_endpoint->callback())
 		{
-			print_r($response);
+			$this->load->model('users');
+			
+			// Test to see if user exists
+			if ($this->users->get_user($response->user_email))
+			{
+				echo $response->user_email . ' exists!';
+			}
+			else
+			{
+				echo $response->user_email . ' does not exist!';
+			}
 		}
 		else
 		{
---
+++ src/application/models/users.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Users extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function get_user($email)
+	{
+		if ($user = $this->mongo_db->where(array('email' => $email)))
+		{
+			if (count($user) == 1)
+			{
+				return TRUE;
+			}
+			else
+			{
+				return FALSE;
+			}
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+
+}
+
+// End of file users.php
