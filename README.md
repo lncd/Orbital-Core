6bf930b15db17a7f6296a2ea1fb1020d1a9e6142
Nick Jackson <nijackson@lincoln.ac.uk>
2012-04-23 15:56:57 +0100

    Licence, and also seeing if Jenkins likes it.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This repository is the Orbital Core, responsible for the deposit, storage, retri
 University of Lincoln
 
 http://orbital.blogs.lincoln.ac.uk
+
+## License
+
+Orbital Core is licenced under the [GNU Affero General Public License][agpl]
+
+[agpl]: https://www.gnu.org/licenses/agpl-3.0.html (GNU Affero General Public License)
--- src/application/controllers/archive.php
+++ src/application/controllers/archive.php
@@ -8,10 +8,26 @@ require APPPATH.'/libraries/Orbital_Controller.php';
  *
  * Stores files in archive.
  *
+ * LICENCE: This file is part of Orbital Core.
+ *
+ * Orbital Core is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Orbital Core. If not, see <http://www.gnu.org/licenses/>.
+ *
  * @package    Orbital
  * @subpackage Core
  * @author     Harry Newton <hnewton@lincoln.ac.uk>
  * @copyright  2012 University of Lincoln
+ * @licence    https://www.gnu.org/licenses/agpl-3.0.html  GNU Affero General Public License
  * @link       https://github.com/lncd/Orbital-Core
  */
 
