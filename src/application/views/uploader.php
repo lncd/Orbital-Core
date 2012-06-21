0cd48aa3466aaab8b3588a6abe0912c657fcfd07
Harry Newton <h-newton@hotmail.com>
2012-06-21 15:30:55 +0100

    Removing redundant code
    + More changes to try and fix upload

--- src/application/views/uploader.php
+++ src/application/views/uploader.php
@@ -11,13 +11,9 @@
 <?php
 
 
-echo form_hidden('licence', $this->input->get('licence'));
+echo '<input type="hidden" name="public" id="public" value="' . $this->input->get('public') . '">';
+echo '<input type="hidden" name="licence" id="licence" value="' . $this->input->get('licence') . '">';
 
-echo form_hidden(array(
-	'name'	=> 'public',
-	'id'	=> 'public',
-	'value'	=> $this->input->get('public'),
-));
 ?>
 
 <div id="file-uploader">		
@@ -53,71 +49,6 @@ echo form_hidden(array(
     // don't wait for the window to load  
     window.onload = createUploader;
     
-    $.getJSON('<?php echo base_url(); ?>licences/licence_json/' + $('#licence').val(), function(data) {
-
-		if (data.allow !== null)
-		{
-			$('#licenceAllowContent').html(data.allow);
-			$('#licenceAllow').show();
-		}
-		
-		if (data.conditions !== null)
-		{
-			$('#licenceConditionsContent').html(data.conditions);
-			$('#licenceConditions').show();
-		}
-		
-		if (data.forbid !== null)
-		{
-			$('#licenceDenyContent').html(data.forbid);
-			$('#licenceDeny').show();
-		}
-		
-		$('#licenceInfoURL').html('<a href="' + data.summary_uri + '" target="_blank">' + data.summary_uri + '</a>');
-		
-	  
-	});
-	
-	$('#licence').change(function(){
-	
-			
-		$.getJSON('<?php echo base_url(); ?>licences/licence_json/' + $('#licence').val(), function(data) {
-		  
-			if (data.allow !== null)
-			{
-				$('#licenceAllowContent').html(data.allow);
-				$('#licenceAllow').show();
-			}
-			else
-			{
-				$('#licenceAllow').hide();
-			}
-			
-			if (data.conditions !== null)
-			{
-				$('#licenceConditionsContent').html(data.conditions);
-				$('#licenceConditions').show();
-			}
-			else
-			{
-				$('#licenceConditions').hide();
-			}
-			
-			if (data.forbid !== null)
-			{
-				$('#licenceDenyContent').html(data.forbid);
-				$('#licenceDeny').show();
-			}
-			else
-			{
-				$('#licenceDeny').hide();
-			}
-			
-			$('#licenceInfoURL').html('<a href="' + data.summary_uri + '" target="_blank">' + data.summary_uri + '</a>');
-		  
-		});
-	  
-	});
   
 </script>
 
